c6007f6bf3bb532f99b56f56971f08c3044765d4
nick860 <nick.glebov000@gmail.com>
2019-11-24 21:00:41 +0200

    d

--- sa.py
+++ sa.py
@@ -1,5 +1,5 @@
 import time
-
+import timeit
 import os
 import sys
 from watchdog.observers import Observer
@@ -29,15 +29,15 @@ class DetectiveFiles(wait):
         self.observer.join()
             
 class TheEvent(hand):
-
+    
     def __init__(self):
         pass
     def on_any_event(self,event):
         if event.is_directory:
              return None
             
-        elif event.event_type=="created" and event.src_path.find("results")==-1:
-            
+        elif event.event_type=="created":
+             t0= time.clock()
              print event.event_type,": ",event.src_path
              print "####################################################"
              thefile=re.split(r"\\+",event.src_path)
@@ -53,35 +53,34 @@ class TheEvent(hand):
                  os.system(myCmd11)
                  print "running virus...."
                  time.sleep(3)
-             myCmd = 'regedit /e /y "C:\myRegAfter.reg" HKEY_CURRENT_USER\Printers\Defaults'
+             myCmd = 'regedit /e /y "C:\myRegAfter.reg" HKEY_CURRENT_USER/'
              os.system(myCmd)
              print "checking the virus..."
              
              time.sleep(3)
              try:
                  words=subprocess.check_output('fc C:\myRegBefore.reg C:\myRegAfter.reg',shell=True)
-                 print words
              except:
                 try:
                  words=subprocess.check_output('fc C:\myRegBefore.reg C:\myRegAfter.reg > results.txt',shell=True)
-                 
                 except:
-                     print "--------------------change-----------"
+                    
                      time.sleep(3)
                      with open(r"C:\Python27\Scripts\results.txt",'r') as input_file:
                        words=input_file.read()
                    
-                       print "compring between files..." 
-                       time.sleep(3)      
-                         
-                       conclude=words.replace('\n',"")
-                       #print conclude
-                       tov=re.findall('\w+\W+\SMYREGAFTER.REG[^*[].*?]+',conclude)
-                       for i in tov:
-                           if len(i)<50:    
-                            print "-------------CHANGE: ",i
-
-                
+             print "compring between files..."
+             t1 = time.clock() - t0
+             print("Time elapsed: ", t1 - t0)
+             time.sleep(3)      
+             print "##############=RESULT=################"
+             conclude=words.replace('\n',"")
+               
+             tov=re.findall('\w+\W+\SMYREGAFTER.REG[^*[].*?]+',conclude)
+             for i in tov:
+                if len(i)<50:    
+                  print "-------------CHANGE: ",i
+             exit()   
 class RegDetective(wait):
     pass
     #f=os.system('cmd /k "ipconig"')Computer\HKEY_LOCAL_MACHINE\SYSTEM\WPA
@@ -93,9 +92,10 @@ def timer():
     os._exit(0)
         
 if __name__=="__main__":
-    
 
-    myCmd = 'regedit /e /y "C:\myRegBefore.reg" HKEY_CURRENT_USER\Printers\Defaults'
+ 
+ 
+    myCmd = 'regedit /e /y "C:\myRegBefore.reg" HKEY_CURRENT_USER/'
     os.system(myCmd)
     
     Files32=DetectiveFiles("C:\Python27\Scripts")
@@ -110,4 +110,3 @@ if __name__=="__main__":
     
     Files32.join()
     Reg.join()
-
