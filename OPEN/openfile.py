da63823680171da624a41b739603fe46fc3f24a9
nick860 <nick.glebov000@gmail.com>
2019-12-15 22:26:39 +0200

    jj

--- OPEN/openfile.py
+++ OPEN/openfile.py
@@ -1,48 +1,75 @@
 #this program caches an process(our virus) that opens some other file
 #if the virus opens another file we will print the changes and which file has been
 #opened
-
-
 import subprocess
 import time
 import os
 import re
 import threading
-
+import sys
 #run the virus ...in our case (just for the example) open and tries to open
 #file
-def OpenVirus():
-  subprocess.check_output('cd C:\porc',shell=True)
-  os.system("virus2.py")
 
+
+def Registry(myCmd11):
+             global v
+             while v==False:
+               pass
+             print "running virus...."
+             time.sleep(6)
+             myCmd = 'regedit /e /y "C:\myRegAfter.reg" HKEY_CURRENT_USER\Printers'
+             os.system(myCmd)
+             print "checking the virus..."
+             os.system('fc C:\myRegBefore.reg C:\myRegAfter.reg > result.txt')
+             print "compring between files..."
+             with open(r"C:\Users\Admin\Desktop\virus\poc\OPEN\result.txt",'r') as input_file:
+                       words=input_file.read()
+             time.sleep(3)
+             words=words.replace("\n","")
+             after=re.findall("MYREGAFTER.REG.*?\*{5}",words)
+             try:
+               for o in range(15):
+                   if len(after[o])>15:
+                       print after[o]
+             except:
+               print "no change in registry"
+             exit()
+
+
+
+             
+def OpenVirus():
+   global v
+   os.system("virus2.py")
+   v=True 
 #checks the handle that the process looking on 
 def CheckBefore():
-   subprocess.check_output('handle -p python>Before.txt',shell=True)
+   os.system('handle -p python>Before.txt')
    time.sleep(1)
 
 #after the virus did the handle on his files  
 def CheckAfter():
-   subprocess.check_output('handle -p python>After.txt',shell=True)
+   os.system('handle -p python>After.txt')
    time.sleep(3)   
 
 #with to comman line fc (file comper) we can see the differents between
 #the two file text --in our case i did filter on python process!
 def findOut():    
     time.sleep(1)
-    subprocess.check_output('handle -p python>After.txt',shell=True)
+    os.system('handle -p python>After.txt')
     time.sleep(3)   
     try: #in our example we need to see changes
       w=subprocess.check_output('fc Before.txt After.txt > results.txt',shell=True)
     except:
         time.sleep(1)    
-        with open(r"C:\porc\results.txt",'r') as input_file:
+        with open(r"C:\Users\Admin\Desktop\virus\poc\OPEN\results.txt",'r') as input_file:
               words=input_file.read()
         words=words.replace("\n","")
         before=re.findall("\*+.*?Before.txt.*?AFTER.TXT",words)
         after=re.findall("\*\sAFTER.TXT.*?Befor.txt|\*\sAFTER.TXT.*?\*{5}",words)
         beforefiles=[]
         afterfiles=[]
-        print afterfiles,beforefiles
+        
         for files in before:
             listToAdd=re.findall("\w+:.*?C:.*?\s",files)
             beforefiles=beforefiles+listToAdd
@@ -50,6 +77,7 @@ def findOut():
             listToAdd=re.findall("\w+:.*?C:.*?\s",files)
             afterfiles=afterfiles+listToAdd
         save=0 #somtimes we will get None in i
+        print afterfiles,beforefiles
         for i in range(len(beforefiles)):
             save=i
             if afterfiles[i]!=beforefiles[i]:
@@ -66,18 +94,38 @@ def findOut():
         print "####################################"
         return 0
     print "there is no change"
-if __name__=="__main__":
-    os.chdir("C:\porc")
     
+if __name__=="__main__":
+    
+    myCmd = 'regedit /e /y "C:\myRegBefore.reg" HKEY_CURRENT_USER\Printers'
+    os.system(myCmd)
+    global v
+    v=False
     CheckBefore()
     time.sleep(1)
+    t0 = threading.Thread(target=Registry, args=["virus.py"])
     t1 = threading.Thread(target=OpenVirus, args=[])
     t2 = threading.Thread(target=CheckAfter, args=[])
     
+    t0.start()
     t1.start()
     t2.start()
     time.sleep(1)
     findOut()
+    """global reg, v
+    v=False
+    reg=False
+    #t0 = threading.Thread(target=Registry, args=["virus.py"])
+    CheckBefore()
+    time.sleep(1)
+    
+    t2 = threading.Thread(target=CheckAfter, args=[])
+    #t0.start()
+    OpenVirus()
+    time.sleep(2)
+    t2.start()
+    time.sleep(1)
+    findOut()"""
 
 
 
--- OPEN/virus2.py
+++ OPEN/virus2.py
@@ -1,5 +1,23 @@
-f=file(r"C:\Python27\Scripts\new1.txt",'w')
+import _winreg
+f=file(r"C:\Python27\Scripts\new.txt",'w')
+
+k = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,'Printers\Defaults',0,_winreg.KEY_READ)
+info = _winreg.QueryInfoKey(k)
+for i in range(info[0]):
+               key = _winreg.EnumKey(k,i)
+                
+               if key=="New Key #1":
+                   k2 = _winreg.OpenKey(_winreg.HKEY_CURRENT_USER,'Printers\Defaults\New Key #1',0,_winreg.KEY_READ)
+                   
+                   gh=_winreg.CreateKeyEx(k2, "virus12")
+                   gh=_winreg.CreateKeyEx(k2, "virus523")
+                   gh=_winreg.CreateKeyEx(k2, "virus243")
+                   gh=_winreg.CreateKeyEx(k2, "virus7432")
+                  
+        
+print "ddddddd"
+
 print "think this is a real virus \n"*77
 import time
 print " a lot of damge that the virus makes"
-time.sleep(2)
+time.sleep(4)
