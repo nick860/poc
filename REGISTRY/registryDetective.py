aea206089c546d48e24abc472f077f0637aad842
nick860 <nick.glebov000@gmail.com>
2019-12-15 21:18:39 +0200

    new

--- REGISTRY/registryDetective.py
+++ REGISTRY/registryDetective.py
@@ -11,47 +11,28 @@ from watchdog.events import FileSystemEventHandler as hand
 from watchdog.events import PatternMatchingEventHandler as listen
 from threading import Thread as wait
 import subprocess
-
 import re
-
 def Registry(myCmd11):
-                 os.system(myCmd11)
-                 print "running virus...."
-                 time.sleep(3)
-             myCmd = 'regedit /e /y "C:\myRegAfter.reg" HKEY_CURRENT_USER/'
+             os.system(myCmd11)
+             print "running virus...."
+             time.sleep(6)
+             myCmd = 'regedit /e /y "C:\myRegAfter.reg" HKEY_CURRENT_USER\Printers'
              os.system(myCmd)
              print "checking the virus..."
-             
-             time.sleep(3)
-             try:
-                 words=subprocess.check_output('fc C:\myRegBefore.reg C:\myRegAfter.reg',shell=True)
-             except:
-                try:
-                 words=subprocess.check_output('fc C:\myRegBefore.reg C:\myRegAfter.reg > results.txt',shell=True)
-                except:
-                    
-                     time.sleep(3)
-                     with open(r"C:\Python27\Scripts\results.txt",'r') as input_file:
-                       words=input_file.read()
-                   
+             os.system('fc C:\myRegBefore.reg C:\myRegAfter.reg > result.txt')
              print "compring between files..."
-             
-             time.sleep(3)      
-             print "##############=RESULT=################"
-             conclude=words.replace('\n',"")
-               
-             tov=re.findall('\w+\W+\SMYREGAFTER.REG[^*[].*?]+',conclude)
-             for i in tov:
-                if len(i)<50:    
-                  print "-------------CHANGE: ",i
-             exit()   
-def timer():
-    time.sleep(5)
-    os._exit(0)
-        
+             with open(r"C:\Users\Admin\Desktop\virus\poc\REGISTRY\result.txt",'r') as input_file:
+                       words=input_file.read()
+             time.sleep(3)
+             words=words.replace("\n","")
+             after=re.findall("MYREGAFTER.REG.*?\*{5}",words)
+             for o in range(15):
+                 if len(after[o])>15:
+                     print after[o]           
+             exit()  
 if __name__=="__main__":
-    #our path to follow----current user------
-    myCmd = 'regedit /e /y "C:\myRegBefore.reg" HKEY_CURRENT_USER/'
+
+    myCmd = 'regedit /e /y "C:\myRegBefore.reg" HKEY_CURRENT_USER\Printers'
     os.system(myCmd)
-    Registry(myCmd11)
-    
+    Registry("virus.py")
+   
